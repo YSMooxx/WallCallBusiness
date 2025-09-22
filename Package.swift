5b3ddf0f5ec4ee12b2c99c827093f05cc01563b2
new <584025430@qq.com>
2025-09-22 17:55:53 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WallCallBusiness",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "WallCallBusiness",
+            targets: ["WallCallBusiness"]),
+    ],
+    dependencies: [
+        // 在这里重新添加你之前的第三方依赖
+        .package(url: "https://github.com/chenmtianlai/myNetTool.git", .upToNextMajor(from: "1.0.9")),
+    ],
+    targets: [
+        .target(
+            name: "WallCallBusiness",
+            dependencies: [
+                .product(name: "Package_net", package: "myNetTool")]
+        ),
+        .testTarget(
+            name: "WallCallBusinessTests",
+            dependencies: ["WallCallBusiness"]
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WallCallBusiness
+
+WallCallBusiness 用于接入1v1 聊天业务逻辑和数据
+
